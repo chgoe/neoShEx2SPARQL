96112e1c2ba0a40e0275cc0c17e3f9dff1e27c5b
Christoph Göpfert <christoph.goepfert@informatik.tu-chemnitz.de>
2025-08-25 10:53:34 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-const neoshex2sparql = require("./neoshex2sparql/shex2sparql.js")
-const shex2sparql = require("./shex2sparql/shex2sparql.js")
+const shex2sparql = require("./neoshex2sparql/shex2sparql.js")
 
 
 // construct a SPARQL CONSTRUCT query for any instance data of a shape
-let query = shex2sparql("test/shapes/eachof-oneof.shex", "CONSTRUCT");
-console.log(query)
-query = neoshex2sparql("test/shapes/eachof-oneof.shex", "CONSTRUCT");
-console.log(query)
+const query = shex2sparql("../test/shapes/eachof-oneof.shex", "CONSTRUCT");
 
 // OR: construct a SPARQL CONSTRUCT query for instance data of a shape with a certain URI
-//const query = shex2sparql("examples/person.shex", "CONSTRUCT", "https://example.org/John_Doe");
+//const query = shex2sparql("../examples/person.shex", "CONSTRUCT", "https://example.org/John_Doe");
 
 // OR: construct a SPARQL ASK query to verify whether instance data of a shape with a certain URI exists
-//const query = shex2sparql("examples/person.shex", "ASK", "https://example.org/John_Doe");
+//const query = shex2sparql("../examples/person.shex", "ASK", "https://example.org/John_Doe");
 
 // OR: construct a SPARQL SELECT query to select instance data of a shape with a certain URI
-//const query = shex2sparql("examples/person.shex", "SELECT");
+//const query = shex2sparql("../examples/person.shex", "SELECT");
 
 console.log(query);
