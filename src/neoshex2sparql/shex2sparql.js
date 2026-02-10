7caa140cdf6944faa6fb0efd41b9d9d0b6095a3d
Christoph Göpfert <christoph.goepfert@informatik.tu-chemnitz.de>
2026-02-10 21:05:37 +0100

    randomify variable naming, as there may be multiple values given for a certain property

--- src/neoshex2sparql/shex2sparql.js
+++ src/neoshex2sparql/shex2sparql.js
@@ -327,7 +327,7 @@ class SimplifiedShexToSparqlConverter {
     generateVariable(predicate, subject) {
         // Create a unique variable name from both predicate and subject
         const input = `${predicate}_${subject}`;
-        const hash = crypto.randomUUID().substring(0, 8);
+        const hash = crypto.randomUUID();
         return `?litOrObj_${hash}`;
     }
 
